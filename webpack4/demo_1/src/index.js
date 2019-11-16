3bbc313c520c63980e717829a3746b7f51dca1cc
zkTom <15128061822@163.com>
2019-11-16 17:33:54 +0800

    webpack4/demo_1:添加tree-shaking

--- webpack4/demo_1/src/index.js
+++ webpack4/demo_1/src/index.js
@@ -1,3 +1,7 @@
+// tree shaking
+import { cube } from '@/utils.js';
+console.log(cube(8));
+
 import bgImg from '@/assets/img/countrypath.jpg'
 import smallImg from '@/assets/img/logo.png'
 import '@/assets/scss/index.scss';
@@ -5,7 +9,10 @@ import '@/assets/scss/index.scss';
 import homeModule from '@/assets/scss/home.module.scss?module';
 
 import './es6.js'
+// 懒加载
+// 注意：一个模块如果既被同步引入，又被动态引入，则不会被分块打包
 import(/* webpackChunkName: 'injectScript'*/ './config/injectScript.js');
+// import(/* webpackChunkName: 'style'*/ "@/assets/scss/index.lazy.scss");
 let i = 1000;
 function addImg() {
     const img = document.createElement('img'); 
@@ -29,8 +36,7 @@ function lazyLoadStyle() {
         // 在css中导出样式可以use()/unuse()进行懒加载
         // import styles from './assets/scss/index.lazy.css';
         // styles.use(); styles.unse();
-        // 懒加载样式
-        // import(/* webpackChunkName: 'style'*/ "@/assets/scss/index.lazy.scss");
+        
         const div = document.createElement('div');
         div.className = 'li';
         div.innerText = i;
