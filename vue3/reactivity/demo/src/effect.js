b86fba33ef0f60336ec568bca23963058f202326
zkTom <15128061822@163.com>
2020-04-06 18:00:54 +0800

    增加Proxy demo

--- vue3/reactivity/demo/src/effect.js
+++ vue3/reactivity/demo/src/effect.js
@@ -1,15 +1,17 @@
 // targetMap 保存原始对象以及他们的依赖映射
 // key: target val: depsMap
 // depsMap  保存依赖关系
-// key: target对象键名 val: effect数组(又叫做dep)
+// key: target对象键名 val: effect数组(又叫做deps)
 // 完成了 target => key => dep 的关系建立
 const targetMap = new WeakMap()
+// 便于依赖收集使用
 const effectStack = []
 // 依赖追踪
 export function track(target, operationType, key) {
 	const effect = effectStack[effectStack.length - 1]
 	if (effect) {
-		let depsMap = targetMap.get(target)
+        let depsMap = targetMap.get(target)
+        console.log(depsMap);
 		if (depsMap === void 0) {                                                                                                                                     
 			targetMap.set(target, (depsMap = new Map()))
 		}
@@ -24,7 +26,7 @@ export function track(target, operationType, key) {
 		}
 	}
 }
-// 依赖收集
+// 依赖收集/触发
 export function trigger(target, operationType, key) {
 	const depsMap = targetMap.get(target)
 	if (depsMap === void 0) {
@@ -32,15 +34,15 @@ export function trigger(target, operationType, key) {
 	}
 	const effects = new Set()
 	if (key !== void 0) {
-		const dep = depsMap.get(key)
+        const dep = depsMap.get(key)
 		dep &&
 			dep.forEach((effect) => {
 				effects.add(effect)
 			})
-	}
+    }
 	if (operationType === 'add' || operationType === 'set') {
 		const iterationKey = Array.isArray(target) ? 'length' : Symbol('iterate')
-		const dep = depsMap.get(iterationKey)
+        const dep = depsMap.get(iterationKey)
 		dep &&
 			dep.forEach((effect) => {
 				effects.add(effect)
@@ -67,7 +69,7 @@ function run(effect, fn, args) {
 			// fn() 执行过程会完成依赖收集，会用到 effect
 			return fn(...args)
 		} finally {
-			// 完成依赖收集后从池子中扔掉这个 effect
+            // 完成依赖收集后从池子中扔掉这个 effect
 			effectStack.pop()
 		}
 	}
--- vue3/reactivity/demo/src/handle.js
+++ vue3/reactivity/demo/src/handle.js
@@ -5,16 +5,17 @@ import { trigger, track } from './effect.js'
 const isObject = (val) => val !== null && typeof val === 'object'
 const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key)
 
+// receiver 目标对象的代理对象或者继承自代理对象的其他对象
 function createGetter(target, key, receiver) {
 	const res = Reflect.get(target, key, receiver)
-	track(target, 'get', key)
+    track(target, 'get', key)
 	return isObject(res) ? reactive(res) : res
 }
 
 function createSetter(target, key, value, receiver) {
 	const hadKey = hasOwn(target, key)
-	const oldValue = target[key]
-	const result = Reflect.set(target, key, value, receiver)
+    const oldValue = target[key]
+    const result = Reflect.set(target, key, value, receiver)
 	if (!hadKey) {
 		trigger(target, 'add', key)
 	} else if (value !== oldValue) {
